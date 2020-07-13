b85821eccce7975b889c8b4703fe75064c18d7ce
Chris Evans <chrisevans9629@gmail.com>
2020-07-12 20:31:01 -0500

    added file to be built

--- build.py
+++ build.py
@@ -1,6 +1,13 @@
 import os
 import sys
+import io
+import datetime
 
-print('Hello World!')
+dataFile = open('data.txt', 'w')
+
+time = datetime.datetime.now().strftime('%d/%m/%y %I:%M %S %p')
+
+dataFile.write('hello world! ' + time)
 
 print(sys.argv)
+
